7b240a58f8e9e2638e5ab92b1939a0def1f1b3d3
Jesse Barbieri <jsb306@scarletmail.rutgers.edu>
2020-07-31 12:30:51 -0400

    Change background to Blue - test

--- PayState/ViewController.swift
+++ PayState/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .purple
+        self.view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
 
