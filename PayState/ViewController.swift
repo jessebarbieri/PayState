85313ad589624330cf523f02916c400f618da44f
Jesse Barbieri <jsb306@scarletmail.rutgers.edu>
2020-07-31 12:26:53 -0400

    Removed Storyboard

---
+++ PayState/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PayState
+//
+//  Created by Jesse Barbieri on 7/31/20.
+//  Copyright © 2020 JBBP Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PayStateTests/PayStateTests.swift
@@ -0,0 +1,34 @@
+//
+//  PayStateTests.swift
+//  PayStateTests
+//
+//  Created by Jesse Barbieri on 7/31/20.
+//  Copyright © 2020 JBBP Apps. All rights reserved.
+//
+
+import XCTest
+@testable import PayState
+
+class PayStateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
