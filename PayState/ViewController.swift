2d9a2e558293525f96ee4382944acfd3f685c538
Jesse Barbieri <jsb306@scarletmail.rutgers.edu>
2020-07-31 22:54:21 -0400

    Added State Picker and two test options, no constraints

--- PayState/ViewController.swift
+++ PayState/ViewController.swift
@@ -8,14 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
+{
+        
+        let stateArray = ["New Jersey", "New York"]
 
-    override func viewDidLoad() {
+        override func viewDidLoad()
+        {
         super.viewDidLoad()
+        
+        // Background Color
         self.view.backgroundColor = .purple
+        
+        // State Picker
+        let UIPicker: UIPickerView = UIPickerView()
+        
+        UIPicker.delegate = self as UIPickerViewDelegate
+        UIPicker.dataSource = self as UIPickerViewDataSource
+        self.view.addSubview(UIPicker)
+        UIPicker.center = self.view.center
+        
+        
         // Do any additional setup after loading the view.
-    }
-
-
+        }
+        
+        // State Picker Functionality
+        func numberOfComponents(in pickerView: UIPickerView) -> Int
+            {
+               return 1
+            }
+           
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+            {
+               return stateArray.count
+            }
+           
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+            {
+              let row = stateArray[row]
+              return row
+            }
+        
 }
 
